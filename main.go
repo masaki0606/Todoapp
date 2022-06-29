9798552cd7b4cb63b3f2f1b792f16159849aee64
isomasaki <masaki.iso@rakus-partners.co.jp>
2022-06-30 08:37:14 +0900

    todoテーブルの作成

--- main.go
+++ main.go
@@ -7,27 +7,28 @@ import (
 
 func main() {
 	fmt.Println(models.Db)
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testetst"
+	fmt.Println(u)
+
+	u.CretateUser()
 	/*
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
-		u.PassWord = "testetst"
+		u, _ := models.GetUser(1)
 		fmt.Println(u)
 
-		u.CretateUser()
-	*/
+		u.Name = "Test2"
+		u.Email = "test2@example.com"
+		u.UpdateUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
 
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
+		u.DeleteUser()
+		u,_ = models.GetUser(1)
+		fmt.Println(u) */
 
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	u.DeleteUser()
-	u,_ = models.GetUser(1)
-	fmt.Println(u)
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 
 }
