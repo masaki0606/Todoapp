7203aa371f0142671d9d965a561b96fdfc303563
isomasaki <masaki.iso@rakus-partners.co.jp>
2022-07-01 08:58:20 +0900

    delete文の追加

--- app/models/todo.go
+++ app/models/todo.go
@@ -20,3 +20,65 @@ func (u *User) CreateTodo(content string) (err error) {
 	}
 	return err
 }
+
+func GetTodo(id int) (todo Todo, err error) {
+	cmd := `select id,content,user_id,created_at from todos where id = ?`
+	todo = Todo{}
+	err = Db.QueryRow(cmd, id).Scan(&todo.ID, &todo.Content, &todo.UserID, &todo.CreatedAt)
+	return todo, err
+}
+
+func GetTodos() (todos []Todo, err error) {
+	cmd := `select id,content,user_id,created_at from todos`
+	rows, err := Db.Query(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID, &todo.Content, &todo.UserID, &todo.CreatedAt)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	rows.Close()
+	return todos, err
+}
+
+func (u *User) GetTodosByUser(id int) (todos []Todo, err error) {
+	cmd := `select id,content,user_id,created_at from todos where user_id = ?`
+	rows, err := Db.Query(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID, &todo.Content, &todo.UserID, &todo.CreatedAt)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+
+	}
+	rows.Close()
+	return todos, err
+}
+
+func (t *Todo) UpdateTodo() error {
+	cmd := `update todos set content = ?,user_id = ? where id = ?`
+	_, err = Db.Exec(cmd, t.Content, t.UserID, t.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func (t *Todo) DeleteTodo() error {
+	cmd := `delete from todos where id = ?`
+	_, err = Db.Exec(cmd, t.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
--- main.go
+++ main.go
@@ -7,13 +7,16 @@ import (
 
 func main() {
 	fmt.Println(models.Db)
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testetst"
-	fmt.Println(u)
+	/*
+		u := &models.User{}
+		u.Name = "test2"
+		u.Email = "test2@example.com"
+		u.PassWord = "testetst2"
+		fmt.Println(u)
 
-	u.CretateUser()
+		u.CretateUser()
+	*/
+
 	/*
 		u, _ := models.GetUser(1)
 		fmt.Println(u)
@@ -27,8 +30,32 @@ func main() {
 		u.DeleteUser()
 		u,_ = models.GetUser(1)
 		fmt.Println(u) */
+	/*
+		user, _ := models.GetUser(2)
+		user.CreateTodo("First Todo")
+	*/
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("First Todo")
+	/*
+		t, _ := models.GetTodo(1)
+		fmt.Println(t)
+	*/
+	/*
+		user, _ := models.GetUser(4)
+		user.CreateTodo("forth Todo")
+
+		/*
+			todos,_ := models.GetTodos()
+			for _,v := range todos {
+				fmt.Println(v)
+			} */
+	/*
+		user2, _ := models.GetUser(3)
+		todos, _ := user2.GetTodosByUser(2)
+		for _, v := range todos {
+			fmt.Println(v)
+		} */
+
+	t, _ := models.GetTodo(3)
+	t.DeleteTodo()
 
 }
